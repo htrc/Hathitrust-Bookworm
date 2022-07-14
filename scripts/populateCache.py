58b065d7cc43eb4cc77586d1352cef71a09b0768
Deren Kudeki <dkudeki@illinois.edu>
2022-07-14 14:44:48 -0500

    Separate out the stringifying from the encoding

--- scripts/populateCache.py
+++ scripts/populateCache.py
@@ -2,7 +2,9 @@ import requests, urllib, json
 
 def populateCache():
 	params = {"groups":["date_year"],"counttype":"WordsPerMillion","words_collation":"Case_Insensitive","database":"Bookworm2021","search_limits":[{"word":["polka"],"date_year":{"$gte":1760,"$lte":2010},"languages__id":["122"]}],"method":"data","format":"json"}
-	encoded_params = urllib.parse.urlencode(json.dumps(params))
+	json_string = json.dumps(params)
+	print(json_string)
+	encoded_params = urllib.parse.urlencode(json_string)
 	print(encoded_params)
 	results = requests.get('http://localhost:10013?query=' + encoded_params,headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
 	print(results)
