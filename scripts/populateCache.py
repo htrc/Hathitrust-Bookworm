d8dfc3a58d9127ae5810efd75f157c709b211740
Deren Kudeki <dkudeki@illinois.edu>
2022-07-14 14:40:22 -0500

    Try encoding the dict

--- scripts/populateCache.py
+++ scripts/populateCache.py
@@ -1,7 +1,10 @@
-import requests
+import requests, urllib
 
 def populateCache():
-	results = requests.get('http://localhost:10013?query=%7B%22groups%22%3A%5B%22date_year%22%5D%2C%22counttype%22%3A%22WordsPerMillion%22%2C%22words_collation%22%3A%22Case_Insensitive%22%2C%22database%22%3A%22Bookworm2021%22%2C%22search_limits%22%3A%5B%7B%22word%22%3A%5B%22polka%22%5D%2C%22date_year%22%3A%7B%22%24gte%22%3A1760%2C%22%24lte%22%3A2010%7D%2C%22languages__id%22%3A%5B%22122%22%5D%7D%5D%2C%22method%22%3A%22data%22%2C%22format%22%3A%22json%22%7D',headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+	params = {"groups":["date_year"],"counttype":"WordsPerMillion","words_collation":"Case_Insensitive","database":"Bookworm2021","search_limits":[{"word":["polka"],"date_year":{"$gte":1760,"$lte":2010},"languages__id":["122"]}],"method":"data","format":"json"}
+	encoded_params = urllib.parse.urlencode(params)
+	results = requests.get('http://localhost:10013?query=' + encoded_params,headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
 	print(results)
+	print(results.content)
 
 populateCache()
