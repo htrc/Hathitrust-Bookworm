4cc6ec6b5b923c878db83b2d6e4872db21a029ba
Deren Kudeki <dkudeki@illinois.edu>
2022-07-14 15:48:33 -0500

    See if running a loop will make it sequencial

--- scripts/populateCache.py
+++ scripts/populateCache.py
@@ -1,15 +1,13 @@
-import requests, urllib, json
+import requests, json
 
 def populateCache():
-	params = {"groups":["date_year"],"counttype":"WordsPerMillion","words_collation":"Case_Insensitive","database":"Bookworm2021","search_limits":[{"word":["polka"],"date_year":{"$gte":1760,"$lte":2010},"languages__id":["122"]}],"method":"data","format":"json"}
-	json_string = json.dumps(params)
-	print(json_string)
-	encoded_params = urllib.parse.quote_plus(json_string)
-	print(encoded_params)
-	url = 'http://localhost:10013?query=' + json_string
-	print(url)
-	results = requests.get(url,headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
-	print(results)
-	print(results.content)
+	for lang in ['1','2']:
+		params = {"groups":["date_year"],"counttype":"WordsPerMillion","words_collation":"Case_Insensitive","database":"Bookworm2021","search_limits":[{"word":["polka"],"date_year":{"$gte":1760,"$lte":2010},"languages__id":[lang]}],"method":"data","format":"json"}
+		json_string = json.dumps(params)
+		url = 'http://localhost:10013?query=' + json_string
+		print(url)
+		results = requests.get(url,headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+		print(results)
+		print(results.content)
 
 populateCache()
