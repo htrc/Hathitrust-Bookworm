f367454933e967ef6de491008a022b0ccbe60352
Deren Kudeki <dkudeki@illinois.edu>
2022-07-14 14:45:53 -0500

    Trying a different method

--- scripts/populateCache.py
+++ scripts/populateCache.py
@@ -4,7 +4,7 @@ def populateCache():
 	params = {"groups":["date_year"],"counttype":"WordsPerMillion","words_collation":"Case_Insensitive","database":"Bookworm2021","search_limits":[{"word":["polka"],"date_year":{"$gte":1760,"$lte":2010},"languages__id":["122"]}],"method":"data","format":"json"}
 	json_string = json.dumps(params)
 	print(json_string)
-	encoded_params = urllib.parse.urlencode(json_string)
+	encoded_params = urllib.parse.quote_plus(json_string)
 	print(encoded_params)
 	results = requests.get('http://localhost:10013?query=' + encoded_params,headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
 	print(results)
