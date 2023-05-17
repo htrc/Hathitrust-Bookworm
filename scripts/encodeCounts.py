fc709958645601f820ee169a995f1040a4b5d635
Deren Kudeki <dkudeki@illinois.edu>
2023-05-17 11:33:09 -0500

    I want to know both types

--- scripts/encodeCounts.py
+++ scripts/encodeCounts.py
@@ -148,7 +148,7 @@ def encodeH5File(counts,word_dict,vol_dict,output_folder,output_file_size,q):
 			logging.debug(chunk)
 			logging.debug(chunk['count'])
 			logging.debug(type(chunk))
-			logging.debug(chunk['count'])
+			logging.debug(type(chunk['count']))
 			chunk['count'].drop(drop_list,inplace=True)
 			logging.debug("Finished dropping")
 			logging.debug(len(chunk['count'].values))
