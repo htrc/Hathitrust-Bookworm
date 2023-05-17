b2aa0d73be204d3fe62e325be38a9952a74832a5
Deren Kudeki <dkudeki@illinois.edu>
2023-05-17 00:02:04 -0500

    to_string didn't work

--- scripts/encodeCounts.py
+++ scripts/encodeCounts.py
@@ -147,7 +147,6 @@ def encodeH5File(counts,word_dict,vol_dict,output_folder,output_file_size,q):
 
 			logging.debug(drop_list)
 			logging.debug(chunk['count']['6952'])
-			logging.debug(chunk['count']['6952'].to_string())
 			chunk.drop(drop_list,inplace=True)
 			logging.debug("Finished dropping")
 
