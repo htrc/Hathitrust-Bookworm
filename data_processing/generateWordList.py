38771434b6d294d6ccdf84a684b29725f1020f12
Deren Kudeki <dkudeki@illinois.edu>
2025-09-19 20:08:21 -0500

    Setup explicit option for only generating stores to avoid full runs when not needed.
    I think I was explicitly calling each component when needed before, but not generating the wordlist is a basic use-case that should be an option when invoking the main call

--- data_processing/generateWordList.py
+++ data_processing/generateWordList.py
@@ -1,4 +1,4 @@
-import os, sys, time, datetime
+import os, sys, time, datetime, argparse
 import generateStores
 import reduceCounts
 import createWordlist
@@ -37,25 +37,33 @@ def validateInputs(features,data,core_count):
 		print('%s does not exist' % features)
 		return False
 
-def generateWordList(features,data,core_count):
-	#check that features and data are valid locations. If not throw error
-	if data[-1:] != SLASH:
-		data = data + SLASH
+def generateWordList(args):
+	#check that volumes and output are valid locations. If not throw error
+	if args.output[-1:] != SLASH:
+		args.output = args.output + SLASH
 
-	if features[-1:] != SLASH:
-		features = features + SLASH
+	if args.volumes[-1:] != SLASH:
+		args.volumes = args.volumes + SLASH
 
-	start_time = datetime.datetime.now().time()
+	start_time = datetime.datetime.now()
 
-	if validateInputs(features,data,core_count):
-		generateStores.generateStores(features,data,int(core_count))
-		reduceCounts.reduceCounts(data,int(core_count))
-		createWordlist.createWordlist(features,data,int(core_count))
+	if validateInputs(args.volumes,args.output,args.core_count):
+		generateStores.generateStores(args.volumes,args.output,int(args.core_count))
+		if not args.fast:
+			reduceCounts.reduceCounts(args.output,int(args.core_count))
+			createWordlist.createWordlist(args.volumes,args.output,int(args.core_count))
 
-	end_time = datetime.datetime.now().time()
-	print("Start time: " + str(start_time))
-	print("End time: " + str(end_time))
-	print("Run duration: " + str(datetime.datetime.combine(datetime.date.min,end_time)-datetime.datetime.combine(datetime.date.min,start_time)))
+	end_time = datetime.datetime.now()
+	print(f"Start time: {start_time}")
+	print(f"End time: {end_time}")
+	print(f"Run duration: {end_time-start_time}")
 
 if __name__ == "__main__":
-	generateWordList(sys.argv[1],sys.argv[2],sys.argv[3])
+	parser = argparse.ArgumentParser()
+	parser.add_argument("volumes", help="Where you rsync'd the EF files to, and where volumes/listing contains a file called ids.txt that is a copy of htids_hathifile_set_stubby.csv")
+	parser.add_argument("output", help="Directory to write the output to")
+	parser.add_argument("core_count", help="Number of cores to use for parallel processing")
+	parser.add_argument("-f", "--fast", action="store_true", help="Generate stores for database creation, but don't build new wordlist. For relying on existing wordlist.")
+	args = parser.parse_args()
+
+	generateWordList(args)
